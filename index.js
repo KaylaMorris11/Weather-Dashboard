a603194aab5eee65fe1cd20ed72f11655472d4fa
Kayla Morris <moreiskay14@gmail.com>
2021-10-14 11:04:46 -0700

    Started api function

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var searchCity = $("searchCity");
 var searchEl =  $("#search").val();
+var cityLocation = "";
 
 searchCity.addEventListener('click', search);
-var cityLocation = "";
+function searchACity() {
+    cityLocation = $("#search").val();
+}
+
+var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityLocation + "&units=imperial&appid=e41962a063e9be6787e7150171080faf";
+  fetch(requestUrl)
+   .then(function (response) {
+     return response.json();
+   })
+   .then(function (data) {
+
+   }
+
 
