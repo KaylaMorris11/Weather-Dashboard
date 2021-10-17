85a4ba8ce128bd0c0ab1976c4cc917d460ff4bf7
Kayla Morris <moreiskay14@gmail.com>
2021-10-16 23:02:37 -0700

    Figured out how to get date to render

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
 var searchCity = $("#searchCity");
-var searchEl =  $("#search").val();
+var searchEl = $("#search").val();
 
 searchCity.click(function(){
     cityLocation =  $("#search").val();
-    getWeather();
+    getWeather1();
+    // getWeather();
 })
 
 var cityLocation = "";
 
-
-function getWeather() {
+function getWeather1() {
     console.log("starting")
-var url = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityLocation + "&units=imperial&appid=ff27d0fb5cd91d7cb860b0e68030b800";
+var url = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityLocation + "&appid=ff27d0fb5cd91d7cb860b0e68030b800";
   fetch(url)
    .then(function (response) {
      return response.json();
    })
    .then(function (data) {
-       console.log(data);
-    
-
-       
+       //based from project 1
+    $("#currentCityName").text(cityLocation);
+    $("#currentDayDate").text(moment(data.dt).format("MMM/ D/ YYYY"));
+    $("#currentDayTemp").text("Temperature: " +data.main.temp +" F");
+    $("currentDayHumidity").text("Humidity: " +data.main.humidity + "%");
+    $("#currentDayWind").text("Wind: " +data.wind.speed + " MPH");
      })
     };
+
+// function getWeather() {
+//     console.log("starting")
+// var url = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityLocation + "&units=imperial&appid=ff27d0fb5cd91d7cb860b0e68030b800";
+//   fetch(url)
+//    .then(function (response) {
+//      return response.json();
+//    })
+//    .then(function (data) {
+//        console.log(data);
+
+//      })
+//     };
