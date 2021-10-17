f30bd31ecf4260ffa89b678b0824a8b2de7db57c
Kayla Morris <moreiskay14@gmail.com>
2021-10-16 23:58:20 -0700

    Attempting to create past city elements

--- index.js
+++ index.js
@@ -23,11 +23,21 @@ var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityLocation + 
     $("#currentDayDate").text(moment(data).format("MMM/D/ YYYY"));
     $("#currentDayHumidity").text("Humidity: " +data.main.humidity+ "%");
     $("#currentDayTemp").text("Temperature: " +data.main.temp +" F");
-
     $("#currentDayWind").text("Wind: " +data.wind.speed + " MPH");
-     })
-    };
 
+    function pastCities() {
+        var cityEl = document.createElement("p");
+        var cities = $("#search").val();
+        cityEl.textContent(cities);
+        console.log(cities)
+        // cityEl.setAttribute()
+    }
+    pastCities();
+    //There is no UV info because it retired
+     })
+     
+    };
+  
 // function getWeather() {
 //     console.log("starting")
 // var url = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityLocation + "&units=imperial&appid=ff27d0fb5cd91d7cb860b0e68030b800";
