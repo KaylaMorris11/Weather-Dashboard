dc0c7f837d6089cec617f601eb02c1bb3cd2570b
Kayla Morris <moreiskay14@gmail.com>
2021-10-19 18:16:45 -0700

    Added 5 day forcast elements

--- index.js
+++ index.js
@@ -1,52 +1,54 @@
 var searchCity = $("#searchCity");
 var searchEl = $("#search").val();
-
-searchCity.click(function(){
-    cityLocation =  $("#search").val();
-    getWeather1();
-    // getWeather();
-})
-
 var cityLocation = "";
 
+searchCity.click(function () {
+  cityLocation = $("#search").val();
+  getWeather1();
+  get5DayForecast();
+});
+
 function getWeather1() {
-    console.log("starting")
-var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityLocation + "&units=imperial&appid=ff27d0fb5cd91d7cb860b0e68030b800";
+  console.log("starting");
+  var url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityLocation +
+    "&units=imperial&appid=ff27d0fb5cd91d7cb860b0e68030b800";
   fetch(url)
-   .then(function (response) {
-     return response.json();
-   })
-   .then(function (data) {
-       console.log(data)
-       //based from project 1
-    $("#currentCityName").text(cityLocation);
-    $("#currentDayDate").text(moment(data).format("MMM/D/ YYYY"));
-    $("#currentDayHumidity").text("Humidity: " +data.main.humidity+ "%");
-    $("#currentDayTemp").text("Temperature: " +data.main.temp +" F");
-    $("#currentDayWind").text("Wind: " +data.wind.speed + " MPH");
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      //based from project 1
+      $("#currentCityName").text(cityLocation);
+      $("#currentDayDate").text(moment(data).format("MMM/D/ YYYY"));
+      $("#currentDayHumidity").text("Humidity: " + data.main.humidity + "%");
+      $("#currentDayTemp").text("Temperature: " + data.main.temp + " F");
+      $("#currentDayWind").text("Wind: " + data.wind.speed + " MPH");
+    });
+}
 
-    function pastCities() {
-        var cityEl = document.createElement("p");
-        var cities = $("#search").val();
-        cityEl.textContent(cities);
-        console.log(cities)
-        // cityEl.setAttribute()
-    }
-    pastCities();
-    //There is no UV info because it retired
-     })
-     
-    };
-  
-// function getWeather() {
-//     console.log("starting")
-// var url = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityLocation + "&units=imperial&appid=ff27d0fb5cd91d7cb860b0e68030b800";
-//   fetch(url)
-//    .then(function (response) {
-//      return response.json();
-//    })
-//    .then(function (data) {
-//        console.log(data);
-
-//      })
-//     };
+function get5DayForecast() {
+  console.log("starting");
+  var url =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    cityLocation +
+    "&units=imperial&appid=ff27d0fb5cd91d7cb860b0e68030b800";
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var day = 1;
+      console.log($("#dayDate"+day))
+      for(let i = 4; i < data.list.length; i+= 8){
+        $("#dayDate"+day).text(data.list[i].dt_txt)
+        $("#dayTemp"+day).text("Temp: "+ data.list[i].main.temp)
+        day++;
+      }
+      
+    });
+}
+//possibly uv index
