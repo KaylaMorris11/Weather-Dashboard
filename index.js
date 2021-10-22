af9226d8aefd5edbad35d317105ffcee41eab146
Kayla Morris <moreiskay14@gmail.com>
2021-10-21 23:50:35 -0700

    got local storage to work& allowed buttons to render

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
 var searchCity = $("#searchCity");
+var searchHistoryEl = $("#searchHistory0");
 var searchEl = $("#search").val();
-var cityLocation = "";
 
 searchCity.click(function () {
-  cityLocation = $("#search").val().toUpperCase([0]);
-  getWeather1();
-  get5DayForecast();
+  var cityLocation = $("#search")
+  .val()
+  .split(" ")
+  .map(word => {
+    var firstLetter = word[0].toUpperCase()
+    var wordBody = word.substring(1)
+    return firstLetter + wordBody;
+  })
+  .join(" ");
+
+  getWeather1(cityLocation);
+  
 });
 
+searchHistoryEl.click(function (event){
+  var cityName = event.target.innerText
+  getWeather1(cityName);
+})
 
-function getWeather1() {
+function updateSearchHistory(cityLocation) {
+  var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+  // if(!searchHistory){
+  //   searchHistory = [];
+  // }
+
+  //first check if city already exists in SearchHistory
+  
+  searchHistory.push(cityLocation);
+  localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+  console.log(searchHistory)
+  //add button to search history
+  searchHistoryEl.empty();
+  for(let i = 0; i < searchHistory.length; i++){
+    
+    searchHistoryEl.append(`
+    <button>${searchHistory[i]}</button>
+    `)
+  }
+
+}
+//event listener 
+
+
+function getWeather1(cityLocation) {
   console.log("starting");
+
+updateSearchHistory(cityLocation);
+
   var url =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     cityLocation +
@@ -27,10 +67,14 @@ function getWeather1() {
       $("#currentDayHumidity").text("Humidity: " + data.main.humidity + "%");
       $("#currentDayTemp").text("Temperature: " + data.main.temp + " F");
       $("#currentDayWind").text("Wind: " + data.wind.speed + " MPH");
+      $("#currentDayWeather").text("Conditions: " + data.weather[0].description);
+
+      get5DayForecast(cityLocation);
+  
     });
 }
 
-function get5DayForecast() {
+function get5DayForecast(cityLocation) {
   console.log("starting");
   var url =
     "https://api.openweathermap.org/data/2.5/forecast?q=" +
